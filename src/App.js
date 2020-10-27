a773c13901cfa5ce580c857bf1fba361a49799c2
Emre Ergül <emredevsalot@gmail.com>
2020-10-27 10:01:17 +0300

    Added Single Recipe and Static RecipeList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import RecipeList from './RecipeList';
 
 function App() {
-  return null;
+  return <RecipeList />;
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat it.',
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '5:45',
+    instructions: '1. Put paprika on Pork\n2. Put Pork in oven\n3. Eat it.',
+  },
+];
 
 export default App;
