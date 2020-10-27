dbed7bb08deabb3a9ac0578fcde9812e49e978da
Emre Ergül <emredevsalot@gmail.com>
2020-10-27 10:33:59 +0300

    Added IngredientList and Ingredient Components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ const sampleRecipes = [
     servings: 3,
     cookTime: '1:45',
     instructions: '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat it.',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 pounds',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs',
+      },
+    ],
   },
   {
     id: 2,
@@ -19,6 +31,18 @@ const sampleRecipes = [
     servings: 5,
     cookTime: '5:45',
     instructions: '1. Put paprika on Pork\n2. Put Pork in oven\n3. Eat it.',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '9 pounds',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs',
+      },
+    ],
   },
   {
     id: 3,
@@ -26,6 +50,18 @@ const sampleRecipes = [
     servings: 5,
     cookTime: '5:45',
     instructions: '1. Put paprika on Fish\n2. Put Fish in oven\n3. Eat it.',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Fish',
+        amount: '2 pounds',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs',
+      },
+    ],
   },
 ];
 
