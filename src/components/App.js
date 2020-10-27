b987542f4a82fb983a31e127f37f8afba12028c3
Emre Ergül <emredevsalot@gmail.com>
2020-10-27 12:33:56 +0300

    Recipe Add and Delete With Prop Drilling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RecipeList from './RecipeList';
 import '../css/app.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  return <RecipeList recipes={sampleRecipes} />;
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{ id: uuidv4(), name: 'Name', amount: '1 Tbs' }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+  return (
+    <RecipeList
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
+  );
 }
 
 const sampleRecipes = [
   {
-    id: 1,
+    id: uuidv4(),
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
@@ -27,7 +53,7 @@ const sampleRecipes = [
     ],
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: 'Plain Pork',
     servings: 5,
     cookTime: '5:45',
@@ -46,7 +72,7 @@ const sampleRecipes = [
     ],
   },
   {
-    id: 3,
+    id: uuidv4(),
     name: 'Plain Fish',
     servings: 5,
     cookTime: '5:45',
